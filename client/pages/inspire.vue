1e4455083df208bbca6e59a4651d272d812e7137
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-10-29 20:45:51 +0300

    settings app

---
+++ client/pages/inspire.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-container
+    p {{paragraph}}
+    v-btn(color="success" @click="paragraph = 'new Lorem Ipsum'") Нажать
+</template>
+
+<script>
+export default {
+  data: () => ({
+    paragraph: 'Lorem ipsum'
+  }),
+  head () {
+    return { title: 'Title' }
+  }
+}
+</script>
